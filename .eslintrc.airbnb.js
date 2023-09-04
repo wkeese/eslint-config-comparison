79cf6319205f2b7f0f0cdad86ea1b4e4096e778d
Bill Keese <wkeese@jp.ibm.com>
2023-09-04 09:28:47 +0900

    Add "airbnb/hooks" for testing React hooks.

--- .eslintrc.airbnb.js
+++ .eslintrc.airbnb.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": [
     "eslint-config-airbnb",
     "eslint-config-airbnb-typescript",
+    "airbnb/hooks",
 
     // Turns off all code formatting rules, I just want to compare linting rules.
     "prettier",
